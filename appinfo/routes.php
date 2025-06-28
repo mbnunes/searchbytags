eec20641502d5490bd2bc497deac8a2b4c9b96c4
Mauricio Nunes <mutila@gmail.com>
2025-06-28 12:21:26 -0300

    routes

---
+++ appinfo/routes.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'routes' => [
+        // Exemplo futuro de rota GET (não é obrigatório se só usar a busca unificada)
+        [
+            'name' => 'SearchByTags#search',
+            'url' => '/search',
+            'verb' => 'GET'
+        ],
+    ]
+];
