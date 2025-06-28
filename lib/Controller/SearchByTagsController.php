278f7a18927ea63757a45c3dbfaea4d1052c06a9
Mauricio Nunes <mutila@gmail.com>
2025-06-28 20:44:10 -0300

    sera

--- lib/Controller/SearchByTagsController.php
+++ lib/Controller/SearchByTagsController.php
@@ -1,14 +1,25 @@
-<?php
-
-namespace OCA\SearchByTags\Controller;
-
+use OCP\ITagManager;
+use OCP\IUserSession;
+use OCP\Files\IRootFolder;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\Controller;
-use OCP\IRequest;
 
 class SearchByTagsController extends Controller {
-    public function __construct($AppName, IRequest $request) {
-        parent::__construct($AppName, $request);
+    private ITagManager $tagManager;
+    private IUserSession $userSession;
+    private IRootFolder $rootFolder;
+
+    public function __construct(
+        $appName,
+        IRequest $request,
+        ITagManager $tagManager,
+        IUserSession $userSession,
+        IRootFolder $rootFolder
+    ) {
+        parent::__construct($appName, $request);
+        $this->tagManager = $tagManager;
+        $this->userSession = $userSession;
+        $this->rootFolder = $rootFolder;
     }
 
     /**
@@ -16,6 +27,43 @@ class SearchByTagsController extends Controller {
      * @NoCSRFRequired
      */
     public function search(): DataResponse {
-        return new DataResponse(['message' => 'Rota ativa e funcionando!']);
+        $user = $this->userSession->getUser();
+        $folder = $this->rootFolder->getUserFolder($user->getUID());
+
+        $queryTags = $this->request->getParam('tags');
+        $tagNames = array_filter(array_map('trim', explode(',', $queryTags)));
+
+        if (empty($tagNames)) {
+            return new DataResponse(['error' => 'No tags provided'], 400);
+        }
+
+        $filesWithTags = [];
+
+        foreach ($tagNames as $tagName) {
+            $tag = $this->tagManager->getTag($user, $tagName, 'files');
+            if ($tag === null) {
+                return new DataResponse(["error" => "Tag '$tagName' not found"], 404);
+            }
+
+            $fileIds = $this->tagManager->getIdsForTag($tag);
+            $filesWithTags[] = $fileIds;
+        }
+
+        // Interseção entre os arrays de file IDs
+        $matchingFileIds = array_shift($filesWithTags);
+        foreach ($filesWithTags as $tagFileIds) {
+            $matchingFileIds = array_intersect($matchingFileIds, $tagFileIds);
+        }
+
+        // Mapear IDs para caminhos
+        $results = [];
+        foreach ($matchingFileIds as $fileId) {
+            $file = $folder->getById($fileId);
+            if ($file) {
+                $results[] = $file[0]->getPath();
+            }
+        }
+
+        return new DataResponse(['results' => $results]);
     }
 }
