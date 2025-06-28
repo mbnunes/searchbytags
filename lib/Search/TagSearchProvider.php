7e555431c295f794f53d3a8e41299b77423dcb83
Mauricio Nunes <mutila@gmail.com>
2025-06-28 11:18:27 -0300

    ajuste no search

--- lib/Search/TagSearchProvider.php
+++ lib/Search/TagSearchProvider.php
@@ -23,33 +23,91 @@ class TagSearchProvider implements ISearchProvider {
         /** @var ITagManager $tagManager */
         $tagManager = \OC::$server->getTagManager();
         $tags = $tagManager->getTagsForUser($user->getUID());
+
+        $query = trim($query);
+
+        // Detectar modo: AND ou OR
+        $isAndMode = false;
+        if (stripos($query, 'tag:') === 0) {
+            $isAndMode = true;
+            $query = trim(substr($query, 4));
+        } elseif (stripos($query, 'tags:') === 0) {
+            $query = trim(substr($query, 5));
+        } else {
+            // Não é uma busca por etiqueta
+            return new SearchResult([]);
+        }
+
+        $words = preg_split('/\s+/', $query);
         $matchedTagIds = [];
 
-        foreach ($tags as $tag) {
-            if (stripos($tag['name'], $query) !== false) {
-                $matchedTagIds[] = $tag['id'];
+        // Mapeia cada palavra para IDs de tags (parciais, case-insensitive)
+        foreach ($words as $word) {
+            foreach ($tags as $tag) {
+                if (stripos($tag['name'], $word) !== false) {
+                    $matchedTagIds[$word] = $tag['id'];
+                    break;
+                }
             }
         }
 
-        $entries = [];
-        if (!empty($matchedTagIds)) {
-            $objectIds = $tagManager->getObjectsForTags($matchedTagIds, 'files');
-            $rootFolder = \OC::$server->get(IRootFolder::class);
-            $userFolder = $rootFolder->getUserFolder($user->getUID());
+        // Se faltou casar alguma palavra com tag, aborta no modo AND
+        if ($isAndMode && count($matchedTagIds) < count($words)) {
+            return new SearchResult([]);
+        }
 
-            foreach ($objectIds as $fileId) {
+        $entries = [];
+        $rootFolder = \OC::$server->get(IRootFolder::class);
+        $userFolder = $rootFolder->getUserFolder($user->getUID());
+
+        if ($isAndMode) {
+            // Modo AND: interseção
+            $tagIds = array_values($matchedTagIds);
+            $commonObjectIds = $tagManager->getObjectsForTags([$tagIds[0]], 'files');
+
+            for ($i = 1; $i < count($tagIds); $i++) {
+                $objectIds = $tagManager->getObjectsForTags([$tagIds[$i]], 'files');
+                $commonObjectIds = array_intersect($commonObjectIds, $objectIds);
+                if (empty($commonObjectIds)) {
+                    return new SearchResult([]);
+                }
+            }
+
+            foreach ($commonObjectIds as $fileId) {
                 try {
-                    $node = $userFolder->getById($fileId)[0] ?? null;
-                    if ($node) {
+                    $nodes = $userFolder->getById($fileId);
+                    if (!empty($nodes)) {
+                        $node = $nodes[0];
                         $entries[] = new SearchResultEntry(
                             $node->getName(),
                             $node->getPath(),
                             $node->getId(),
                             $node->getMTime(),
                             $node->getSize(),
-                            '',
-                            '',
-                            ''
+                            '', '', ''
+                        );
+                    }
+                } catch (\Throwable $e) {
+                    continue;
+                }
+            }
+        } else {
+            // Modo OR: união
+            $tagIds = array_values($matchedTagIds);
+            $objectIds = $tagManager->getObjectsForTags($tagIds, 'files');
+
+            foreach ($objectIds as $fileId) {
+                try {
+                    $nodes = $userFolder->getById($fileId);
+                    if (!empty($nodes)) {
+                        $node = $nodes[0];
+                        $entries[] = new SearchResultEntry(
+                            $node->getName(),
+                            $node->getPath(),
+                            $node->getId(),
+                            $node->getMTime(),
+                            $node->getSize(),
+                            '', '', ''
                         );
                     }
                 } catch (\Throwable $e) {
@@ -60,4 +118,5 @@ class TagSearchProvider implements ISearchProvider {
 
         return new SearchResult($entries);
     }
+
 }
