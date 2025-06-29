86c480ce80a7355fa2246343493bbb81fabf9e9b
Mauricio Nunes <mutila@gmail.com>
2025-06-29 01:11:49 -0300

    routes

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -2,7 +2,7 @@
 $application = new \OCA\SearchByTags\AppInfo\Application();
 $application->registerRoutes($this, [
     'routes' => [
-        ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
+        ['name' => 'page#index', 'url' => '/apps/search_by_tags', 'verb' => 'GET'],
         ['name' => 'search#getAllTags', 'url' => '/api/tags', 'verb' => 'GET'],
         ['name' => 'search#searchByTag', 'url' => '/api/search', 'verb' => 'GET']
     ]
