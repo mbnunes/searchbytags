c59f8c320af11ea9e7dd5422948a838700843a36
Mauricio Nunes <mutila@gmail.com>
2025-06-29 20:32:49 -0300

    routes

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -1,33 +1,11 @@
 <?php
 return [
     'routes' => [
-        [
-            'name' => 'page.index',
-            'url' => '/',
-            'verb' => 'GET',
-            'controller' => 'PageController',
-            'action' => 'index',
-        ],
-        [
-            'name' => 'page.viewResult',
-            'url' => '/view',
-            'verb' => 'GET',
-            'controller' => 'PageController',
-            'action' => 'viewResult',
-        ],
-        [
-            'name' => 'search.getAllTags',
-            'url' => '/api/tags',
-            'verb' => 'GET',
-            'controller' => 'SearchController',
-            'action' => 'getAllTags',
-        ],
-        [
-            'name' => 'search.searchByTag',
-            'url' => '/api/search',
-            'verb' => 'GET',
-            'controller' => 'SearchController',
-            'action' => 'searchByTag',
-        ],
+        ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
+        ['name' => 'page#viewResult', 'url' => '/result', 'verb' => 'GET'],
+        ['name' => 'search#getAllTags', 'url' => '/api/tags', 'verb' => 'GET'],
+        ['name' => 'search#searchByTag', 'url' => '/api/search', 'verb' => 'GET'],
     ]
 ];
+
+
