5ba8d955576c95dd36884f9e7735dd28b4dcb136
Mauricio Nunes <mutila@gmail.com>
2025-06-29 22:39:16 -0300

    fix

--- js/result.js
+++ js/result.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', async function () {
                 const li = document.createElement('li');
                 li.className = 'file';
 
-                const fileUrl = OC.generateUrl(`/remote.php/webdav/${file.path}/${file.name}`);
+                const fileUrl = OC.generateUrl(`/remote.php/webdav${file.path}/${file.name}`);
                 console.log('File URL:', fileUrl);  // Debug: Verifique a URL do arquivo
 
                 const link = document.createElement('a');
