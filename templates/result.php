a3b26cc15de1d1a6cc7eba3846b366a10f60159f
Mauricio Nunes <mutila@gmail.com>
2025-06-29 21:33:57 -0300

    div errada

--- templates/result.php
+++ templates/result.php
@@ -4,5 +4,5 @@ script('search_by_tags', 'result');
 
 <div class="section">
     <h2>Resultados da Busca por Tags</h2>
-    <div id="results" style="margin-top: 1em;"></div>
+    <div id="tag-results" style="margin-top: 1em;"></div>
 </div>
