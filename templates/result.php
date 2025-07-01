fa9fb04496bdda11c7d643c9835ed78a61d78cae
Mauricio Nunes <mutila@gmail.com>
2025-07-01 08:13:14 -0300

    aff

--- templates/result.php
+++ templates/result.php
@@ -4,14 +4,14 @@ style('files', 'merged'); // Usa o estilo do app files
 style('search_by_tags', 'style');
 ?>
 
-<div class="app-content">
-    <div class="sidebar">
-        <h2>Busca por Tags</h2>
-        <input id="tag-input" type="text" placeholder="Digite as tags" list="tag-suggestions" class="search"/>
-        <datalist id="tag-suggestions"></datalist>
-    </div>
+<div class="search-by-tags-wrapper">
+	<div class="sidebar">
+		<h2>Busca por Tags</h2>
+		<input id="tag-input" type="text" placeholder="Digite as tags" list="tag-suggestions" class="search"/>
+		<datalist id="tag-suggestions"></datalist>
+	</div>
 
-    <div class="app-content-list" id="tag-results">
-        <!-- Os itens de resultado devem ser adicionados aqui via JS -->
-    </div>
+	<div class="app-content-list" id="tag-results">
+		<!-- Resultados aqui -->
+	</div>
 </div>
