b62c3995973dd8c9b91faf2842ee42c36abbdfb0
Mauricio Nunes <mutila@gmail.com>
2025-07-02 00:16:07 -0300

    fix

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -3,7 +3,7 @@ return [
     'routes' => [
         ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
         ['name' => 'page#viewResult', 'url' => '/result', 'verb' => 'GET'],
-        ['name' => 'search#getAllTags', 'url' => '/api/tags', 'verb' => 'GET'],
+        ['name' => 'search#getAllTags', 'url' => '/search/getAllTags', 'verb' => 'GET'],
         ['name' => 'search#searchByTag', 'url' => '/api/search', 'verb' => 'GET'],
     ]
 ];
